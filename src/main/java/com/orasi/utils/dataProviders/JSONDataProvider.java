71ef28ac25015aaffba7838929daff54014c8b5c
Arven <arven@muckdragon.info>
2014-12-30 15:36:41 -0500

    Add a more complex JSON format.

--- src/main/java/com/orasi/utils/dataProviders/JSONDataProvider.java
+++ src/main/java/com/orasi/utils/dataProviders/JSONDataProvider.java
@@ -1,23 +1,18 @@
 package com.orasi.utils.dataProviders;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.orasi.utils.types.IteratorMap;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
- * This is a basic JSON data provider, which will take a JSON file in the
- * following format.
- * 
- * {@code
- *      [
- *          [ "Col1", "Col2", "Col3", "Col4" ],
- *          [ 1, 2, 3, 4 ]
- *      ]
- * }
+ * This is a basic JSON data provider, which will take in a few formats
+ * of JSON file.
  * 
  * @version     12/30/2014
  * @author      Brian Becker
@@ -39,23 +34,65 @@ public class JSONDataProvider {
         this.mapData = Files.readAllBytes(Paths.get(filePath));
     }
     
-    
     /**
      * This gets the test data from a JSON file.  It returns all the data 
-     * as an Iterator of Object[].
+     * as an Iterator of Object[]. It accepts a 2d array JSON file.
+     * 
+     * {@code
+     *      [
+     *          [ "Col1", "Col2", "Col3", "Col4" ],
+     *          [ 1, 2, 3, 4 ]
+     *      ]
+     * }
      * 
      * @version	12/30/2014
      * @author 	Brian Becker
      * @return 	Iterator of Object[]
      * @throws  java.lang.Throwable
      */
-    public Iterator<Object[]> getTestScenarioData() throws Throwable {
+    public Iterator<Object[]> getData() throws Throwable {
         ObjectMapper map = new ObjectMapper();
-        final Iterator<ArrayList<Object>> data = map.readValue(this.mapData, LinkedList.class).listIterator();
+        final Iterator<ArrayList<Object>> data = map.readValue(mapData, LinkedList.class).listIterator();
         return new IteratorMap<ArrayList<Object>, Object[]>(data) {
             @Override
             public Object[] apply(ArrayList<Object> objs) {
                 return objs.toArray();
+            }
+        };
+    }
+
+    /**
+     * This gets the test data from a JSON file.  It returns all the data 
+     * as an Iterator of Object[]. It accepts an associative array and
+     * passes the key name and value to the test function. It is up to the
+     * test function to query the value.
+     * 
+     * {@code
+     *      {
+     *          "TestCase1":
+     *              [ "Col1", "Col2", "Col3", "Col4" ],
+     *          "TestCase2":
+     *              [ 1, 2, 3, 4 ],
+     *          "TestCase3":
+     *              {
+     *                  "A": 1,
+     *                  "B": 2
+     *              }
+     *      }
+     * }
+     * 
+     * @version	12/30/2014
+     * @author 	Brian Becker
+     * @return 	Iterator of Object[]
+     * @throws  java.lang.Throwable
+     */
+    public Iterator<Object[]> getDataMap() throws Throwable {
+        ObjectMapper map = new ObjectMapper();
+        final JsonNode root = map.readValue(mapData, JsonNode.class);
+        return new IteratorMap<String, Object[]>(root.fieldNames()) {
+            @Override
+            public Object[] apply(String o) {
+                return new Object[] { o, root.get(o) };
             }
         };
     }
