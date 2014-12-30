4c16db362e47054b6ce8dd8de322627ee379c686
Arven <arven@muckdragon.info>
2014-12-30 11:54:19 -0500

    Preliminary JSON data provider, not tested.

--- src/main/java/com/orasi/utils/dataProviders/JSONDataProvider.java
+++ src/main/java/com/orasi/utils/dataProviders/JSONDataProvider.java
@@ -1,13 +1,19 @@
 package com.orasi.utils.dataProviders;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.LinkedList;
 //import org.codehaus.jackson.JsonParser;
 
 public class JSONDataProvider {
     
     // File path of JSON data
-    private String filePath;
+    private final byte[] mapData;
     
     /**
      * This creates the JSON Data provider with a given file path.
@@ -15,9 +21,10 @@ public class JSONDataProvider {
      * @param       filePath                file path of the JSON file
      * @version     12/30/2014
      * @author      Brian Becker
+     * @throws      java.lang.Throwable
      */
-    public JSONDataProvider(String filePath) {
-        this.filePath = filePath;        
+    public JSONDataProvider(String filePath) throws Throwable {
+        this.mapData = Files.readAllBytes(Paths.get(filePath));
     }
     
     
@@ -29,12 +36,13 @@ public class JSONDataProvider {
      * @version	12/30/2014
      * @author 	Brian Becker
      * @return 	Iterator of Object[]
+     * @throws  java.lang.Throwable
      */
-    public static Iterator<Object[]> getTestScenarioData(String filePath){
-   
-        Iterator<Object[]> o = Collections.emptyListIterator();
-        return o;
-
+    public Iterator<Object[]> getTestScenarioData(String filePath) throws Throwable {
+        ObjectMapper map = new ObjectMapper();
+        Collection<Object[]> list = new LinkedList<>();
+        Iterator<Object[]> data = map.readValue(this.mapData, Iterator.class);
+        return data;
     }
 
 }
