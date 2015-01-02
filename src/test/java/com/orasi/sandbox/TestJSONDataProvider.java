9a9e12dc229ff109eff8c57f854b9fe0e8f24970
Arven <arven@muckdragon.info>
2015-01-02 09:57:27 -0500

    Fixed up test locations

--- src/test/java/com/orasi/sandbox/TestJSONDataProvider.java
+++ src/test/java/com/orasi/sandbox/TestJSONDataProvider.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.orasi.unit.utils.dataProviders;
+package com.orasi.sandbox;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.orasi.utils.Constants;
