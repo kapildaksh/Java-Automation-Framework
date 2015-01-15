3911ad2fc4b96e91c7ea7c748222b3525510ecf1
Arven <arven@muckdragon.info>
2015-01-15 14:08:38 -0500

    Beginnings of the Spark Framework mockserver

--- src/main/java/com/orasi/arven/sandbox/MockNewsServer.java
+++ src/main/java/com/orasi/arven/sandbox/MockNewsServer.java
@@ -1,27 +1,17 @@
 package com.orasi.arven.sandbox;
 
-import spark.Request;
-import spark.Response;
-import spark.Route;
+import sparkfive.Request;
+import sparkfive.Response;
+import sparkfive.Route;
+import static sparkfive.Spark.*;
 
 public class MockNewsServer {
     public static void main( final String[] args ) throws Exception {
-            spark.Spark.get(new Route("/hello") {
-
-                @Override
-                public Object handle(Request rqst, Response rspns) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-            
-            });
-        //( x, y ) -> x + y;
-        //spark.Spark.get("/hello", new spark.Route() {
-
-       //     @Override
-       //     public Object handle(Request rqst, Response rspns) throws Exception {
-        //        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        //    }
-            
-       // });
+        get("/hello", new Route() {
+            @Override
+            public Object handle(Request req, Response res) {
+                return "Hello Plain Text";
+            }
+        });
     }
 }
