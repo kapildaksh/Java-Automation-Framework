5a178f0b9c92f908b163209659e7c55d7964249d
Arven <arven@muckdragon.info>
2015-01-22 10:35:52 -0500

    Documentation, hiding values

--- src/main/java/com/orasi/utils/rest/ExpectedResponse.java
+++ src/main/java/com/orasi/utils/rest/ExpectedResponse.java
@@ -17,27 +17,71 @@ import org.testng.Assert;
 public abstract class ExpectedResponse {
     
     @JsonIgnore
-    public List<Patch> ignores = new LinkedList<Patch>();
+    private final List<Patch> ignores = new LinkedList<Patch>();
     @JsonIgnore
-    public List<Patch> patches = new LinkedList<Patch>();
+    private final List<Patch> patches = new LinkedList<Patch>();
     
+    /**
+     * Return the Expected Response in text format.
+     * 
+     * @return expected response
+     */
     public abstract String expected();
+    
+    /**
+     * Return the Returned Response or real response, in text format.
+     * 
+     * @return returned response
+     */
     public abstract String returned();
         
+    /**
+     * Travel to the given path using Jackson-style path notation.
+     * For instance, you may path(0, 1, "Name") to traverse a 2D
+     * array and index the "Name" element in the selected object.
+     * 
+     * @param path Integer and String array pointing to location.
+     * @return 
+     */
     public ExpectedPath path(Object... path) {
         return new ExpectedPath(JsonPointer.fromPath(path), ignores, patches);
     }
     
+    /**
+     * Travel to the given path using JSON-Pointer style path notation.
+     * For instance, you may at("/0/1/Name") to index into a 2D array
+     * and the "Name" element in the selected object.
+     * 
+     * @param path Json Pointer String
+     * @return 
+     */
     public ExpectedPath at(String path) {
         return new ExpectedPath(path, ignores, patches);
     }    
 
-    public ExpectedResponse clear() {
+    /**
+     * Clear out all of the changes which have been done to the expected
+     * response. This is called only internally, after the verification
+     * step is done.
+     * 
+     * @return this
+     */
+    private ExpectedResponse clear() {
         this.ignores.clear();
         this.patches.clear();
         return this;
     }
     
+    /**
+     * Verify this Expected Response. There is no return value, it acts as
+     * an assertion if the Expected Response and the newly returned response
+     * differ. Being able to use slightly variant expected responses without
+     * having to manually input all the test cases is the purpose for the
+     * "path" and "at" methods. You can traverse the ExpectedPath, and use
+     * this interface to remove or change expectations. If successful, these
+     * expected changes are erased and the expectations are ready for another
+     * new use.
+     */
     public void verify() {
         if(expected() == null)
             Assert.fail("Nothing expected! Failed to load response.");
