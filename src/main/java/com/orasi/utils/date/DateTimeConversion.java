6f904668ada5a95d12ee1579c860a32c919822e0
Justin Phlegar <Justin.Phlegar@orasi.com>
2015-06-01 19:07:15 -0400

    Adding deprecation notes

--- src/main/java/com/orasi/utils/date/DateTimeConversion.java
+++ src/main/java/com/orasi/utils/date/DateTimeConversion.java
@@ -8,10 +8,6 @@ import java.util.Date;
 import java.util.Locale;
 
 public class DateTimeConversion {
-
-	public DateTimeConversion() {
-		// TODO Auto-generated constructor stub
-	}
 	
 	public static String convert(String date, String fromFormat, String toFormat){
 		SimpleDateFormat dateFormat = new SimpleDateFormat(fromFormat,Locale.ENGLISH);
@@ -31,18 +27,27 @@ public class DateTimeConversion {
 	    return dateFormat.format(date);
 	}
 	
+	/**
+	 * 
+	 * @param daysOut - Number of days from current date to get date for
+	 * @param format - Desired format of date. Samples are "MM/dd/yyyy", "MMMM dd, yyyy", and "dd-MM-yy" 
+	 * @return String future date in desired format
+	 */
 	public static String getDaysOut(String daysOut, String format){
 		String date = "";
 		DateFormat dateFormat = new SimpleDateFormat(format);
 		Calendar cal = Calendar.getInstance();
-		String currentDate = dateFormat.format(cal.getTime());
 		cal.add(Calendar.DATE, Integer.parseInt(daysOut));
 		String convertedDate=dateFormat.format(cal.getTime());    
 		
 		return convertedDate;
 	}
 	
-	
+	/**
+	 * Marked for Deprecation 6/1/2015 <br>
+	 * New method {@link #getDaysOut(String, String) getDaysOut} <br>
+	 * Sample: {@code getDaysOut(5,"MM/dd/yyyy")} 
+	 */
 	@Deprecated
 	public static String ConvertToDate(String daysOut){
 		String date = "";
@@ -55,6 +60,11 @@ public class DateTimeConversion {
 		return convertedDate;
 	}
 	
+	/**
+	 * Marked for Deprecation 6/1/2015 <br>
+	 * New method {@link #getDaysOut(String, String) getDaysOut} <br>
+	 * Sample: {@code getDaysOut(5,"MM/dd/yyyy")} 
+	 */
 	@Deprecated
 	public String ConvertToDateMMDDYY(String daysOut){
 		String date = "";
@@ -67,12 +77,23 @@ public class DateTimeConversion {
 		return convertedDate;
 	}
 	
+	/**
+	 * Marked for Deprecation 6/1/2015 <br>
+	 * New methods: <br>
+	 * {@link #convert(String date, String fromFormat, String toFormat)   } <br>
+	 * {@link #convert(Date date, String toFormat) }<br>
+	 * Sample: {@code convert("06/01/2015", "MM/dd/yyyy", "MMMM dd, yyyy)} 
+	 */
 	@Deprecated
 	public static String format(String date, String format){
 		return new SimpleDateFormat(format).format(date);
 	}
 
-
+	/**
+	 * Marked for Deprecation 6/1/2015 <br>
+	 * New method {@link #getDaysOut(String, String) getDaysOut} <br>
+	 * Sample: {@code getDaysOut(5,"yyyy-MM-dd")} 
+	 */
 	@Deprecated
 	public String ConvertToDateYYYYMMDD(String daysOut){
 		String date = "";
