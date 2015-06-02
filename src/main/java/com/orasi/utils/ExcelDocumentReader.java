e40284b46fef70338dd1cf80c98292e6f2654566
Justin Phlegar <Justin.Phlegar@orasi.com>
2015-06-02 12:22:42 -0400

    Removed Scenario log

--- src/main/java/com/orasi/utils/ExcelDocumentReader.java
+++ src/main/java/com/orasi/utils/ExcelDocumentReader.java
@@ -85,7 +85,7 @@ public class ExcelDocumentReader {
 		
 		for (int i = startRow; i <= totalRows; i++, ci++) {
 			cj = 0;
-			System.out.println("Test Scenario: " + getCellData(i, 1));
+			//System.out.println("Test Scenario: " + getCellData(i, 1));
 			for (int j = startCol; j < totalCols; j++, cj++) {					
 				tabArray[ci][cj] = getCellData(i, j);
 			//	System.out.println(getCellData(0,j) + ": " + tabArray[ci][cj]);
